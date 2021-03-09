82912b1c934c2f710e10fc4fdcc0eb244ee1989d
Sara Rios <sharitarios@gmail.com>
2021-03-09 10:07:49 -0500

    Commit 01: se creó el proyecto primerProyecto con un directorio css que tiene dos archivos css y un archivo index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es"> 
+<head>
+    <title>Proyecto Herocu</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" content="text/css" href="css/normalize.css"/>
+    <link rel="stylesheet" content="text/css" href="css/estilos.css"/>
+</head>
+<body>
+    <div id="envoltura">
+    <header>
+        <h1>Programaci&oacute;n Web</h1>
+    </header>
+    <div id="main">
+        <p>
+            En este curso aprender&aacute;s a programar p&aacute;ginas web usando git.
+
+            <?php 
+                $curso = "Construcción de elementos de software 2";
+                echo "</br></br> Estás en el curso $curso."
+            ?>
+        </p>
+     </div>
+    </div>
+</body>
+
+</html>
